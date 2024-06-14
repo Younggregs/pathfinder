847ab36b6c81c8571339d6f424c2e59986c18ce7
retzam <dretzam@gmail.com>
2024-06-14 13:34:15 +0100

    Create get_location_code util

--- flightfinder/lib/utils.py
+++ flightfinder/lib/utils.py
@@ -5,6 +5,12 @@ def get_flights(airline, date, origin, destination):
     return airline(date, origin, destination).find_flights()
 
 def format_date_with_dash(date_string):
-        datetime_object = datetime.strptime(date_string, "%d.%m.%Y")
-        formatted_date = datetime_object.strftime("%Y-%m-%d")
-        return formatted_date
+    datetime_object = datetime.strptime(date_string, "%d.%m.%Y")
+    formatted_date = datetime_object.strftime("%Y-%m-%d")
+    return formatted_date
+
+def get_location_code(locations, name):
+    for location in locations:
+        if location["name"].upper() == name.upper():
+            return location["code"]
+    return None
