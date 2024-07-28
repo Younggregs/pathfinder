b40510c58978e18861a3606cec0c2ed197989cd8
retzam <dretzam@gmail.com>
2024-07-28 08:40:33 +0100

    Update cache flights cron

--- flightfinder/tasks.py
+++ flightfinder/tasks.py
@@ -4,28 +4,35 @@ from flightfinder.lib.merlin import Merlin
 from flightfinder.models import CachePath
 from pathfinder.celery import app
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @app.task
 def cache_flights():
-
     paths_to_cache = CachePath.objects.all()
     for path in paths_to_cache:
         # Iterate over the next 14 days
         for day in range(14):
 
-            date = datetime.now() + timedelta(days=day)
-            formatted_date = date.strftime("%d.%m.%Y")
+            try:
+                date = datetime.now() + timedelta(days=day)
+                formatted_date = date.strftime("%d.%m.%Y")
 
-            # Summon Merlin with the date for the current iteration
-            merlin = Merlin(path.origin.code, path.destination.code, formatted_date)
+                # Summon Merlin with the date for the current iteration
+                merlin = Merlin(path.origin.code, path.destination.code, formatted_date)
 
-            # Perform magic
-            merlin.prepare_magic()
-            merlin.abracadabra()
-            merlin.persist_magic()
+                # Perform magic
+                merlin.prepare_magic()
+                merlin.abracadabra()
+                merlin.persist_magic()
 
-            # Delay for 30 seconds after each iteration
-            time.sleep(30)
+                # Delay for 30 seconds after each iteration
+                time.sleep(30)
+            except Exception as e:
+                logger.debug("Error running cache flights: %s", e)
+                continue
 
 
 app.conf.beat_schedule = {
